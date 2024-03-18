1d2c00dab0894165e5552ceca6c5ca8cf5f07d3b
Luiz Gustavo <luiz.gustavo@mobyan.com.br>
2024-03-18 17:38:36 -0300

    Atualização de metodo

--- scripts/global.js
+++ scripts/global.js
@@ -1,23 +1,21 @@
-const simulador = {
-    selecioneVeiculo: 'car',
-    nome: '',
-    cor: '',
-    ano: '',
-    constructor: function() {
-        this.selecioneVeiculo = 'car';
-        this.nome = '';
-        this.cor = '';
-        this.ano = '';
+class Simulador {
+    constructor(nome, cor, ano) {
+        this.selecioneVeiculo = 'Carro';
+        this.nome = 'nome';
+        this.cor = 'cor';
+        this.ano = 'ano';
         this.atualizarVeiculo();
-    },
-    atualizarVeiculo: function() {
+    }
+
+    atualizarVeiculo() {
         this.selecioneVeiculo = document.getElementById("selecioneVeiculo").value;
         this.nome = document.getElementById("nomeInput").value;
         this.cor = document.getElementById("corInput").value;
         this.ano = document.getElementById("anoInput").value;
         this.mostrarVeiculo();
-    },
-    mostrarVeiculo: function() {
+    }
+
+    mostrarVeiculo() {
         let veiculoIcon;
         switch(this.selecioneVeiculo) {
             case "carro":
@@ -36,6 +34,6 @@ const simulador = {
         const imagemVeiculo = document.getElementById("imagemVeiculo");
         imagemVeiculo.innerHTML = `<p>${veiculoIcon}</p><p>Nome: ${this.nome}</p><p>Cor: <span style="color:${this.cor};">${this.cor}</span></p><p>Ano: ${this.ano}</p>`;
     }
-};
+}
 
-simulador.constructor();
+const simulador = new Simulador();
